407e86d7508a8ce934ac49790f1302b545f9c58d
twentyonedot <twentyonedot@gmail.com>
2021-09-01 15:39:33 +0530

    First commit

---
+++ src/pages/metatest1.js
@@ -0,0 +1,16 @@
+import Seo from '../components/Seo/index';
+import React from 'react';
+
+const MetaTest1 = () => {
+  return (
+    <div>
+      <Seo
+        title="MetaTest1"
+        image="https://images.pexels.com/photos/479454/pexels-photo-479454.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+      />
+      <h1>MetaTest1</h1>
+    </div>
+  );
+};
+
+export default MetaTest1;
---
+++ src/pages/metatest2.js
@@ -0,0 +1,16 @@
+import Seo from '../components/Seo/index';
+import React from 'react';
+
+const MetaTest2 = () => {
+  return (
+    <div>
+      <Seo
+        title="MetaTest2"
+        image={`https://www.gatsbyjs.org/gatsby-icon.png`}
+      />
+      <h1>MetaTest2</h1>
+    </div>
+  );
+};
+
+export default MetaTest2;
