ef46e2970298aa311837ac5f6ce8251c51fedd29
twentyonedot <twentyonedot@gmail.com>
2021-09-02 23:13:49 +0530

    medium import fix

--- src/components/Seo/index.js
+++ src/components/Seo/index.js
@@ -3,7 +3,7 @@ import Helmet from 'react-helmet';
 import { graphql, useStaticQuery } from 'gatsby';
 import PropTypes from 'prop-types';
 
-function Seo({ description, lang, meta, title, postTitle, image, type }) {
+function Seo({ description, lang, meta, postTitle, image, title, type }) {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -66,44 +66,7 @@ function Seo({ description, lang, meta, title, postTitle, image, type }) {
           content: metaImage,
         },
       ].concat(meta)}
-    >
-      <meta charset="utf-8" />
-      <title>The Open Graph protocol</title>
-      <meta
-        name="description"
-        content="The Open Graph protocol enables any web page to become a rich object in a social graph."
-      />
-      <script type="text/javascript">
-        var _sf_startpt = new Date().getTime();
-      </script>
-      <meta property="og:title" content="Open Graph protocol" />
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://ogp.me/" />
-      <meta property="og:image" content="https://ogp.me/logo.png" />
-      <meta property="og:image:type" content="image/png" />
-      <meta property="og:image:width" content="300" />
-      <meta property="og:image:height" content="300" />
-      <meta property="og:image:alt" content="The Open Graph logo" />
-      <meta
-        property="og:description"
-        content="The Open Graph protocol enables any web page to become a rich object in a social graph."
-      />
-      <meta
-        prefix="fb: https://ogp.me/ns/fb#"
-        property="fb:app_id"
-        content="115190258555800"
-      />
-      <link
-        rel="alternate"
-        type="application/rdf+xml"
-        href="https://ogp.me/ns/ogp.me.rdf"
-      />
-      <link
-        rel="alternate"
-        type="text/turtle"
-        href="https://ogp.me/ns/ogp.me.ttl"
-      />
-    </Helmet>
+    />
   );
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,11 @@ import { Link } from 'gatsby';
 import Seo from '../components/Seo/index';
 
 const IndexPage = () => {
-  React.useEffect(() => {
-    const head = document.querySelector('head');
-    head.setAttribute('prefix', 'og: http://ogp.me/ns#');
-  }, []);
   return (
     <main>
       <Seo
         title="MetaTest1"
         image="https://images.pexels.com/photos/159304/network-cable-ethernet-computer-159304.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-        type="article"
       />
       Home
       <Link to="/metatest1">Meta Test1</Link>
