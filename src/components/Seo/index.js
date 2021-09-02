db5fa6c878db8a3cb0864614b862caa10e6c03e6
twentyonedot <twentyonedot@gmail.com>
2021-09-02 23:25:31 +0530

    medium import fix

--- src/components/Seo/index.js
+++ src/components/Seo/index.js
@@ -66,7 +66,44 @@ function Seo({ description, lang, meta, postTitle, image, title, type }) {
           content: metaImage,
         },
       ].concat(meta)}
-    />
+    >
+      <meta charset="utf-8" />
+      <title>The Open Graph protocol</title>
+      <meta
+        name="description"
+        content="The Open Graph protocol enables any web page to become a rich object in a social graph."
+      />
+      <script type="text/javascript">
+        var _sf_startpt = new Date().getTime();
+      </script>
+      <meta property="og:title" content="Open Graph protocol" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://ogp.me/" />
+      <meta property="og:image" content="https://ogp.me/logo.png" />
+      <meta property="og:image:type" content="image/png" />
+      <meta property="og:image:width" content="300" />
+      <meta property="og:image:height" content="300" />
+      <meta property="og:image:alt" content="The Open Graph logo" />
+      <meta
+        property="og:description"
+        content="The Open Graph protocol enables any web page to become a rich object in a social graph."
+      />
+      <meta
+        prefix="fb: https://ogp.me/ns/fb#"
+        property="fb:app_id"
+        content="115190258555800"
+      />
+      <link
+        rel="alternate"
+        type="application/rdf+xml"
+        href="https://ogp.me/ns/ogp.me.rdf"
+      />
+      <link
+        rel="alternate"
+        type="text/turtle"
+        href="https://ogp.me/ns/ogp.me.ttl"
+      />
+    </Helmet>
   );
 }
 
